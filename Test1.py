ecd99fa3c37a78b0d2c7724c1a6c71c350c832e5
NACHO9999 <Nachoetcheverry@gmail.com>
2023-02-01 03:56:54 -0300

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -2,6 +2,6 @@ def merge_lists(list1, list2):
     merged_list = list2
     for i in list1:
       if (i not in merged_list):
-        merged_list.append(i)
+        merged_list.append(i).
     print(sorted(merged_list))
 merge_lists([1,2,3,4,5],[1,6,4,3,7,8,]) 
