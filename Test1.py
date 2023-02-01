85083f3ce2442bf29083ac65fdf9a5af82c9117c
Nacho Etcheverry <nachoetcheverry@gmail.com>
2023-02-01 03:56:17 -0300

    Added Python testing

---
+++ Test1.py
@@ -0,0 +1,7 @@
+def merge_lists(list1, list2):
+    merged_list = list2
+    for i in list1:
+      if (i not in merged_list):
+        merged_list.append(i)
+    print(sorted(merged_list))
+merge_lists([1,2,3,4,5],[1,6,4,3,7,8,]) 
